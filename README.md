abdc287cba488d392415010eeff441bcd6854aaf
Andrew Benson <adbenson@cmu.edu>
2014-10-25 03:46:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+crashdevblog-example
+==================
+
+The example crashdevblog created for the Intro Web Applications workshop for ScottyLabs' CrashCourse 2014.
+
+This website is created in Flask, backed by the worst database ever: RAM. It's notable, though, that it includes an AJAX request, I guess.
+
+If you're in the Intro Web Applications workshop, this is not the right repository. This is the example website, not the working website.
+
+The extra files (like the Procfile) are needed to get this to work on Heroku.
+
+You can find this website hosted at crashdevblog.herokuapp.com. You could probably crash this easily, or do bad things, but please don't.
---
+++ main.py
@@ -0,0 +1,56 @@
+import json
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
+
+# app configuration
+app = Flask(__name__)
+app.debug = True
+
+# rudimentary, hacky database
+blogEntries = [] # this is a time-ordered list of "entries"
+
+def findUTCTimeString():
+  currDT = datetime.utcnow()
+  return currDT.strftime("%I:%M %p (UTC) %b %d, %Y")
+
+def createEntry(author, title, text):
+  return {"author": author,
+          "title": title,
+          "text": text,
+          "datetime": findUTCTimeString()}
+
+class Entry(object):
+  def __init__(self, author, title, text):
+    self.author = author
+    self.title = title
+    self.text = text
+    self.date = datetime.utcnow()
+
+# routing
+@app.route("/")
+def homePage():
+  return render_template("index.html", entries = blogEntries)
+
+@app.route("/about")
+def aboutPage():
+  return render_template("about.html")
+
+@app.route("/create")
+def createPage():
+  return render_template("create.html")
+
+@app.route("/newpost", methods=["POST"])
+def newpost():
+  author = request.form["author"]
+  title = request.form["title"]
+  text = request.form["text"]
+  blogEntries.insert(0, createEntry(author, title, text))
+  return redirect(url_for("homePage"))
+
+@app.route("/refreshEntries")
+def refreshEntries():
+  return json.dumps(blogEntries)
+
+# start the app if we're directly running this file
+if __name__ == "__main__":
+  app.run()
